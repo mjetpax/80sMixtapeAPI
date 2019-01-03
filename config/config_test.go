245341c25490996b7ab85ce2c35ca5f4eb380268
Michael Babcock <mjetpax@gmail.com>
2019-01-02 23:20:36 -0800

    update: config unit test

--- config/config_test.go
+++ config/config_test.go
@@ -8,7 +8,20 @@ import (
 
 func TestConfig(t *testing.T) {
 
-	// InitEnv()
+	InitEnv()
 
 	assert.NotEqual(t, Env.DBHost, "", "`config.Env.DBHost` should not be empty.")
+	assert.NotEqual(t, Env.DBPort, "", "`config.Env.DBPort` should not be empty.")
+	assert.NotEqual(t, Env.DBName, "", "`config.Env.DBName` should not be empty.")
+	assert.NotEqual(t, Env.DBUser, "", "`config.Env.DBUser` should not be empty.")
+	assert.NotEqual(t, Env.DBPass, "", "`config.Env.DBPass` should not be empty.")
+	assert.NotEqual(t, Env.DBConn, "", "`config.Env.DBConn` should not be empty.")
+}
+
+func TestConfigEnvVars(t *testing.T) {
+
+	expected := "default test value"
+	actual := GetEnvVar("TEST_KEY", "default test value")
+	description := "`config.GetEnvVar()` should output default if no env var exists."
+	assert.Equal(t, actual, expected, description)
 }
